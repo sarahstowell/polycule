01b790a8b391b9130f57d14dc5e615f83756713b
sarahstowell <sarah@baldock.me>
2016-07-17 00:02:41 +0100

    More fixes

--- public/signup.js
+++ public/signup.js
@@ -1,9 +1,9 @@
-window.alert("signup.js read");
-
 d3.select("#photoSelect")
 	.on("change", photoSelect);
 
 function photoSelect() {
+
+   window.alert("photoSelect function run");
 
   var imgsrc = null;
   var file    = document.getElementById('photoSelect').files[0];
@@ -164,6 +164,7 @@ function initialize() {
 
 // Signup form validation
 var validateForm = function() {
+    window.alert("ValidateForm function run");
 	d3.select(".inputBox").style("border", "1px solid gray");
 	d3.select("#errorMessage").text("");		
 	if (!document.forms["signup"]["username"].value) {
