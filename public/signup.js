c6073a734b807deca127f8d6d58f55ab4e471a41
sarahstowell <sarah@baldock.me>
2016-07-18 21:51:17 +0100

    Fixes

--- public/signup.js
+++ public/signup.js
@@ -239,7 +239,7 @@ d3.select("#photoTypeNone").on("click", photoTypeNone);
 
 
 var locationInput = document.getElementById('location');
-var searchBox = new google.maps.places.SearchBox(locationInput, { types: ['(cities)'], region:'EU', bounds: defaultBounds });
+var searchBox = new google.maps.places.SearchBox(locationInput, { types: ['(cities)'], region:'EU' });
 //map.controls[google.maps.ControlPosition.TOP_LEFT].push(locationInput);
 
 
