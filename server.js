c85f54a6a67cad43575b563b8289188c5fab2f89
sarahstowell <sarah@baldock.me>
2016-07-19 14:09:26 +0100

    Fix

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ passport.deserializeUser((id, done)=>{
 passport.use('facebook', new FacebookStrategy({
 		clientID        : process.env.FACEBOOK_ID,
 		clientSecret    : process.env.FACEBOOK_SECRET,
-		callbackURL     : 'polyculeuk.herokuapp.com/login/facebook/callback',
+		callbackURL     : 'http://polyculeuk.herokuapp.com/login/facebook/callback',
 		profileFields	  : ['id', 'name', 'emails', 'location', 'picture.width(225)'],
 		passReqToCallback: true
 	},  
