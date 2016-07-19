f826535fc6bcb3276b36d28770c6b3c0813520f9
sarahstowell <sarah@baldock.me>
2016-07-20 00:20:41 +0100

    Fix

--- server.js
+++ server.js
@@ -144,13 +144,14 @@ passport.use('facebook', new FacebookStrategy({
 // SESSIONS ==========================================
 app.use(cookieParser());
 
-
-var sessionMiddleware = session({
-    store: new pgSession({
+var sessionStore = new pgSession({
         //pg : pg,                                  // Use global pg-module 
         conString : process.env.POSTGRES_CONNECTION_STRING, // Connect using something else than default DATABASE_URL env variable 
         tableName : 'sessionstore'               // Use another table-name than the default "session" one 
     }),
+
+var sessionMiddleware = session({
+    store: sessionStore,
     name: 'sessionId',
     secret: '1234567890QWERTY',
     resave: true,
