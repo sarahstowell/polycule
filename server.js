15620bbfbe8b4529c6d11e234a3b339084a768b6
sarahstowell <sarah@baldock.me>
2016-07-20 01:51:38 +0100

    Test

--- server.js
+++ server.js
@@ -371,7 +371,7 @@ io.sockets.on('connection', function(socket){
   console.log('a user connected: ');
   console.log(JSON.stringify(socket.request.user.id));
   console.log('session: ');
-  console.log(JSON.stringify(socket.request.session));
+  console.log(JSON.stringify(socket.id));
    
    // Initial data request (WORKING)
    socket.on('dataRequest', function() {
