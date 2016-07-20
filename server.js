7d02a161a800fbca53cdd2cc80e1a0fe1cd0f8de
sarahstowell <sarah@baldock.me>
2016-07-20 11:02:23 +0100

    Fix

--- server.js
+++ server.js
@@ -429,13 +429,13 @@ io.sockets.on('connection', function(socket){
         });
         */
         
-        /*
+     updateLinks() {
   	    db.any("SELECT * FROM links WHERE confirmed = 1 OR sourceid = "+socket.request.user.id+" OR targetid = "+socket.request.user.id+" ORDER BY id", [true]).then(function(links) { //filter unconfirmed links which are not relevant to current user
 			io.emit('linksUpdate', links);
 			console.log("Updated link data sent");		
 	    }).catch(function (error) {  console.log("ERROR:", error); });
-	    */
-	//}
+
+	}
 	
 	function updateNodes() {
   	    db.any("SELECT * FROM nodes ORDER BY id", [true]).then(function(nodes) { 
