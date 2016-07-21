3a9e9c6a2dca25bd5f2fb5c27182a58c6531b6b1
sarahstowell <sarah@baldock.me>
2016-07-21 12:48:44 +0100

    Fix

--- public/polycule.js
+++ public/polycule.js
@@ -90,9 +90,9 @@ socket.on('nodesAndLinks', function(dataPackage) {
 	    */
 	    
 	    for (i=0; i<nodesUpdate.length; i++) {
-	        nodesUpdate[i].fixed = nodes[arrayObjectIndexOf(nodesUpdate, i, "id")].fixed;
-	        nodesUpdate[i].x = nodes[arrayObjectIndexOf(nodesUpdate, i, "id")].x;
-	        nodesUpdate[i].y = nodes[arrayObjectIndexOf(nodesUpdate, i, "id")].y;
+	        nodesUpdate[i].fixed = nodes[arrayObjectIndexOf(nodes, nodesUpdate[i].id, "id")].fixed;
+	        nodesUpdate[i].x = nodes[arrayObjectIndexOf(nodes, nodesUpdate[i].id, "id")].x;
+	        nodesUpdate[i].y = nodes[arrayObjectIndexOf(nodes, nodesUpdate[i].id, "id")].y;
 	    }
 	    
 	    
@@ -112,9 +112,9 @@ socket.on('nodesAndLinks', function(dataPackage) {
 	    //if (fixedid) { nodesUpdate[arrayObjectIndexOf(nodesUpdate, fixedid, "id")].fixed = 1; }
 	    
 		for (i=0; i<nodesUpdate.length; i++) {
-	        nodesUpdate[i].fixed = nodes[arrayObjectIndexOf(nodesUpdate, i, "id")].fixed;
-	        nodesUpdate[i].x = nodes[arrayObjectIndexOf(nodesUpdate, i, "id")].x;
-	        nodesUpdate[i].y = nodes[arrayObjectIndexOf(nodesUpdate, i, "id")].y;
+	        nodesUpdate[i].fixed = nodes[arrayObjectIndexOf(nodes, nodesUpdate[i].id, "id")].fixed;
+	        nodesUpdate[i].x = nodes[arrayObjectIndexOf(nodes, nodesUpdate[i].id, "id")].x;
+	        nodesUpdate[i].y = nodes[arrayObjectIndexOf(nodes, nodesUpdate[i].id, "id")].y;
 	    }    
 	    
 	    
