f30460cfdec1b517b2217679d1dee0af3dcf64a4
sarahstowell <sarah@baldock.me>
2016-07-25 19:12:40 +0100

    Fix

--- server.js
+++ server.js
@@ -396,7 +396,7 @@ app.post('/signup/facebook', upload.single('profilePic'), function (req, res, ne
 
 app.get('/join', function(req, res) {
     res.render('signup');
-}
+});
 
 app.get('/', function (req, res) {
 
