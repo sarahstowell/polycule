3b966d3b7cb6f846434b656ec45e339fb8b5d15b
sarahstowell <sarah@baldock.me>
2016-07-31 21:12:32 +0100

    test

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ var upload = multer({
         s3: s3,
         //dirname: '/',
         bucket: 'polycule',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        region: 'ireland',
+        //secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        //accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        //region: 'Ireland',
         //acl: 'public-read',
         //metadata: function (req, file, cb) {
         //    cb(null, {fieldName: "Photo"});
