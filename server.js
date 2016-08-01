27383e3a3eea72e1ac3f5f48799df18ffe86dabf
sarahstowell <sarah@baldock.me>
2016-08-01 12:11:45 +0100

    test

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ var storage = multerS3({
 var upload = multer({ storage: storage });
 // ---------------------------------------------------------------------------------------
 
-AWS.config.update(/*{accessKeyId: 'akid', secretAccessKey: 'secret'}*/{region: 'eu-west-1'});
+AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: 'eu-west-1'});
 var s3 = new AWS.S3()
 
 function uploadFile(remoteFilename, buffer) {
