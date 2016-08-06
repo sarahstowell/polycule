0530d3d4cefc95fd68e7793f49f7141d766b09ce
sarahstowell <sarah@baldock.me>
2016-08-06 10:24:17 +0100

    Test photo delete

--- server.js
+++ server.js
@@ -443,17 +443,10 @@ app.post('/update/photo', upload.single('photo'), function(req, res) {
 	db.one("SELECT photo FROM nodes WHERE id=$1", [req.body.id])
 	.then(function(oldPhoto) {
 	    // Update with new photo details
-		db.one("UPDATE nodes SET (photo, photocoords) = ($2, $3) WHERE id = $1 returning photo", [req.body.id, photourl, photocoords])
-		.then(function(oldPhoto2) {
-	        console.log("From select statement: "+JSON.stringify(oldPhoto));
-	        console.log("From update statement: "+JSON.stringify(oldPhoto2));
-	        
-	        //photoRemove(oldPhoto);
-	        
-	        
-	        // Delete old photo
-	        // Refresh nodes data
-	        io.emit('callToUpdateNodes');
+		db.one("UPDATE nodes SET (photo, photocoords) = ($2, $3) WHERE id = $1", [req.body.id, photourl, photocoords])
+		.then(function() {
+	        photoRemove(oldPhoto); // Delete old photo
+	        io.emit('callToUpdateNodes'); // Refresh nodes data
 		})
 		.catch(function(err) {
 	        console.log(err);
