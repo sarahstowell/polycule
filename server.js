440863f43a821369b61bfc1b31b4c5de73f8889b
sarahstowell <sarah@baldock.me>
2016-08-30 22:35:10 +0100

    test

--- server.js
+++ server.js
@@ -408,6 +408,8 @@ app.post('/signup/facebook', upload.single('profilePic'), function (req, res, ne
 	    signupType = "signup";
 	}
 	
+	console.log(signupType);
+	
 	var dbString;
 	if (signupType = "join") { dbString = "UPDATE nodes (name, username, location, description, photo, photocoords, member, invited) = (${name}, ${username}, ${location}, ${description}, ${photo}, ${photocoords}, ${member}, null) WHERE id = "+req.session.inviteId+"returning *"; }
 	else { dbString = "INSERT INTO nodes (name, username, location, description, photo, photocoords, member) VALUES (${name}, ${username}, ${location}, ${description}, ${photo}, ${photocoords}, ${member}) returning id"; }
