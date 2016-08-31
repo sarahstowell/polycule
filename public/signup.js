9fd1202fde27d630093e22a6747079e1f7cb7a3b
sarahstowell <sarah@baldock.me>
2016-08-31 16:55:56 +0100

    test

--- public/signup.js
+++ public/signup.js
@@ -209,7 +209,6 @@ var photoTypeCustom = function() {
 }
 
 var photoTypeNone = function() {
-window.alert("hello");
 	d3.select("#photoAreaFacebook").style("display", "none");
 	d3.select("#photoArea").style("display", "none");
 	d3.select("#photoSelect").property("disabled", true);
