02b4b2c4044cfcc794cede061344ad889796a940
sarahstowell <sarah@baldock.me>
2016-09-16 22:26:32 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -1098,12 +1098,15 @@ socket.on('nodesAndLinks', function(dataPackage) {
 
     // ===== Link Requests =====
     
-/*
+
     function viewModel() {
+    
+    
     
     var self = this;
     
     
+    ko.mapping.fromJS(linkRequests, {}, self);
     
     self.confirmLink = function() { 
         //linkRequests.remove(this);
@@ -1123,33 +1126,10 @@ socket.on('nodesAndLinks', function(dataPackage) {
     };
     }
     
-    ko.mapping.fromJS(linkRequests, viewModel);
-  */  
+    //ko.mapping.fromJS(linkRequests, viewModel);
+
     
-    var viewModel = {
-    
-    confirmLink: function() { 
-        //linkRequests.remove(this);
-        //linkRequests.splice(i, 1); // Delete link from link requests
-		if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link requests remain, dehighlight link request button  
-			        
-		// Send link confirmation to server
-		socket.emit('linkConfirm', this.id);
-    },
-    denyLink: function() { 
-        //linkRequests.remove(this);
-        //linkRequests.splice(this.linkRequests2.id, 1); // Delete Link for link requests
-		if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link request remain, dehighlight link request button
-			
-		// Send link delete to server
-		socket.emit('linkDelete', this.id);
-    }
-    }
-    
-    ko.mapping.fromJS(linkRequests, viewModel);
-    
-    
-    ko.applyBindings(viewModel);
+    ko.applyBindings(new viewModel());
     
 
 
