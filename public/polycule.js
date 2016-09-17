a6ce4c22ef718f9c2b3fd2075a491ddec2fc19e9
sarahstowell <sarah@baldock.me>
2016-09-17 23:32:06 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -73,7 +73,7 @@ socket.on('nodesAndLinks', function(dataPackage) {
     getLinkRequests();
     
     
-    
+    /*
     function viewModel() {
     
         var self = this;
@@ -90,7 +90,24 @@ socket.on('nodesAndLinks', function(dataPackage) {
 			socket.emit('linkDelete', this.id);
 		};
     }
-    ko.applyBindings(new viewModel());
+    */
+    
+    var viewModel =  {
+    
+        linkRequests: ko.observableArray(linkRequests),
+        
+        confirmLink: function() { 
+			if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link requests remain, dehighlight link request button  
+			socket.emit('linkConfirm', this.id);
+		},
+		denyLink: function() { 
+			if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link request remain, dehighlight link request button
+			// Send link delete to server
+			socket.emit('linkDelete', this.id);
+		}
+    }
+    
+    ko.applyBindings(viewModel);
     
     
     
