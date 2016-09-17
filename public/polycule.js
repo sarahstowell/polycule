b6ab9b6be7cacd6c1ca4535cea6e8960f456b56e
sarahstowell <sarah@baldock.me>
2016-09-17 23:03:50 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -21,6 +21,26 @@ function arrayObjectIndexOf(myArray, searchTerm, property) {
 // On receiving data from the server, build visualisation
 socket.on('nodesAndLinks', function(dataPackage) { 
 
+
+    function viewModel() {
+    
+        var self = this;
+        
+        self.linkRequests = ko.observableArray(linkRequests);
+        
+        self.confirmLink = function() { 
+			if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link requests remain, dehighlight link request button  
+			socket.emit('linkConfirm', this.id);
+		};
+		self.denyLink = function() { 
+			if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link request remain, dehighlight link request button
+			// Send link delete to server
+			socket.emit('linkDelete', this.id);
+		};
+    }
+    ko.applyBindings(new viewModel());
+
+
     // Node index of current user
     var loggedin = dataPackage.userid;
     
@@ -58,7 +78,8 @@ socket.on('nodesAndLinks', function(dataPackage) {
         // Collect unconfirmed links which are not requested by current user, for link request folder
         linkRequests = links.filter(function(d) { return d.confirmed === 0 && d.requestor !== loggedin; });
         linkRequests.map(function(d) { d.requestorname = nodes[arrayObjectIndexOf(nodes, d.requestor, "id")].name; d.requestorusername = nodes[arrayObjectIndexOf(nodes, d.requestor, "id")].username;});
-        ko.mapping.fromJS(linkRequests, {}, viewModel);
+        //ko.mapping.fromJS(linkRequests, {}, viewModel);
+        if (viewModel) { viewModel.linkRequests(linkRequests); }
         
         // Highlight button red if there are link requests
         if (linkRequests.length > 0) { d3.select("#linkButton").attr("fill", "red"); } else { d3.select("#linkButton").attr("fill", "black"); }  
@@ -1124,7 +1145,7 @@ var viewModel = ko.mapping.fromJS(linkRequests, mapping);
 //use this as our model bindings
 ko.applyBindings(viewModel);
     */
-    
+    /*
     function viewModel() {
 		var self = this;
 	
@@ -1144,6 +1165,24 @@ ko.applyBindings(viewModel);
     
     //ko.mapping.fromJS(linkRequests, {}, viewModel);
     
+    ko.applyBindings(new viewModel());
+    */
+    function viewModel() {
+    
+        var self = this;
+        
+        self.linkRequests = ko.observableArray(linkRequests);
+        
+        self.confirmLink = function() { 
+			if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link requests remain, dehighlight link request button  
+			socket.emit('linkConfirm', this.id);
+		};
+		self.denyLink = function() { 
+			if (linkRequests.length === 0) { d3.select("#linkButton").attr("fill", "black"); }  // If no more link request remain, dehighlight link request button
+			// Send link delete to server
+			socket.emit('linkDelete', this.id);
+		};
+    }
     ko.applyBindings(new viewModel());
 
     
