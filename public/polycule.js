91f1763f1f8dcfac794756b5382b892384be6deb
sarahstowell <sarah@baldock.me>
2016-09-17 23:12:03 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -63,7 +63,7 @@ socket.on('nodesAndLinks', function(dataPackage) {
         linkRequests = links.filter(function(d) { return d.confirmed === 0 && d.requestor !== loggedin; });
         linkRequests.map(function(d) { d.requestorname = nodes[arrayObjectIndexOf(nodes, d.requestor, "id")].name; d.requestorusername = nodes[arrayObjectIndexOf(nodes, d.requestor, "id")].username;});
         //ko.mapping.fromJS(linkRequests, {}, viewModel);
-        if (viewModel) { viewModel.linkRequests(linkRequests); }
+        //if (viewModel) { viewModel.linkRequests(linkRequests); }
         
         // Highlight button red if there are link requests
         if (linkRequests.length > 0) { d3.select("#linkButton").attr("fill", "red"); } else { d3.select("#linkButton").attr("fill", "black"); }  
@@ -73,7 +73,7 @@ socket.on('nodesAndLinks', function(dataPackage) {
     
     
     
-        function viewModel() {
+    function viewModel() {
     
         var self = this;
         
