478630127d9d5464c5460926b5293b30d612c5cd
sarahstowell <sarah@baldock.me>
2016-09-25 18:20:30 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -169,13 +169,13 @@ socket.on('nodesAndLinks', function(dataPackage) {
 		};
 	
         // Nodes
-        /*
+        
         self.nodes = ko.observableArray(nodes);
         self.activeNode = ko.observable(active_node);
         self.activeNodeData = ko.computed(function() {
             return self.nodes().filter(function(d) { return d.id === self.activeNode(); });
         });
-        */
+        
         
         // Links
         self.links = ko.observableArray(links);
