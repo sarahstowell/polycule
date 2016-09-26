bbd5739748773af3005907dbd6fb6173ed6757bf
sarahstowell <sarah@baldock.me>
2016-09-26 12:41:07 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -191,7 +191,9 @@ socket.on('nodesAndLinks', function(dataPackage) {
         self.links = ko.observableArray(links);
         self.activeLink = ko.observable(active_link);
         self.activeLinkData = ko.computed(function() {
-            return self.links().filter(function(d) { return d.id === self.activeLink(); });
+        	var i = self.links().filter(function(d) { return d.id === self.activeLink(); });
+        	
+            return i[0];
         });
         self.linksEditing = ko.observable(false);
     }
