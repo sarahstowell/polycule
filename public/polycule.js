3181a4bb8d11a0d76c960d342b7a89ed3bb982d9
sarahstowell <sarah@baldock.me>
2016-09-29 22:48:16 +0100

    test

--- public/polycule.js
+++ public/polycule.js
@@ -119,6 +119,7 @@ socket.on('nodesAndLinks', function(dataPackage) {
     function ViewModel(linkData, emailData, nodeData, loggedin, months) {
         var self = this;
         self.user = loggedin;
+        self.links = ko.observableArray(links);
         // Link Requests
         self.linkRequests = ko.observableArray(linkData);
         self.confirmLink = function() { 
@@ -357,7 +358,6 @@ socket.on('nodesAndLinks', function(dataPackage) {
         };
         
         // Links
-        self.links = ko.observableArray(links);
         self.activeLink = ko.observable(active_link);
         self.months = months;
         self.activeLinkData = ko.computed(function() {
