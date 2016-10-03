63eae3f69b269112f987a5e52704322449291ca8
sarahstowell <sarah@baldock.me>
2016-10-03 19:03:22 +0100

    test

--- public/signup.js
+++ public/signup.js
@@ -33,9 +33,9 @@ var validateForm = function(formtype) {
     // do something with cropped base64 image here
         
         document.getElementById("croppedPhoto").value = base64Image;
-        //var blob = new Blob([base64Image], { type: "image/png,base64"});
+        var blob = new Blob([base64Image], { type: "image/png,base64"});
         var fd = new FormData(document.forms[0]);
-        fd.append("croppedPhoto", base64Image);
+        fd.append("croppedPhoto", blob);
     });
     
     
