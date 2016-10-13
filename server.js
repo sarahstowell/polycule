f14683b693bf77d9fc409fae6a195e768cebab74
sarahstowell <sarah@baldock.me>
2016-10-13 22:14:48 +0200

    test

--- server.js
+++ server.js
@@ -395,6 +395,7 @@ app.post('/signup', /*upload.single('profilePic'), */
          function (req, res, next) {
     
     if (req.body.croppedPhoto) { console.log(JSON.stringify(req.body.croppedPhoto)); }
+    if (req.body.hello) { console.log(req.body.hello); }
     //console.log("file1a: "+JSON.stringify(req.files.profilePic[0].filename));
     if (req.files.croppedPhoto && req.files.croppedPhoto[0]) { console.log(req.files.croppedPhoto[0].filename) }
 
