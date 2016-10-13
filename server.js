6e1163b179946ab7a9f4728d416eed094e10df91
sarahstowell <sarah@baldock.me>
2016-10-13 21:58:24 +0200

    test

--- server.js
+++ server.js
@@ -394,11 +394,8 @@ app.post('/signup', /*upload.single('profilePic'), */
          
          function (req, res, next) {
     
-	console.log("file10: "+JSON.stringify(req.files));
-    console.log("file1a: "+JSON.stringify(req.files.profilePic[0].filename));
-    //console.log("file2a: "+JSON.stringify(req.files['croppedPhoto'].filename));
-    //console.log("file1b: "+JSON.stringify(req.files[0].filename));
-    //console.log("file2b: "+JSON.stringify(req.files[1].filename));
+    if (req.body.croppedPhoto) { console.log(JSON.stringify(req.body.croppedPhoto)); }
+    //console.log("file1a: "+JSON.stringify(req.files.profilePic[0].filename));
     if (req.files.croppedPhoto && req.files.croppedPhoto[0]) { console.log(req.files.croppedPhoto[0].filename) }
 
 	if (req.body.photoType === 'custom' && req.files.profilePic[0]) { 
